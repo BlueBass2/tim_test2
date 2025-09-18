8195d12ec420beb30ca66ec6abb586e068907e70
Paul1_Huang <156768984+BlueBass2@users.noreply.github.com>
2025-09-18 22:39:09 +0800

    Add files via upload

---
+++ AutoTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public float CalculateBMI()
+        {
+            float result = 0;
+            return result;
+        }
+
+        public void Save(string userInputFileName)
+        {
+        }
+
+        public DataTable GetUserData(string userInput, SqlConnection connection)
+        {
+            DataTable dt = new DataTable();
+            return dt;
+        }
+    }
+}
